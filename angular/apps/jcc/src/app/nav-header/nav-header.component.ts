88471bc61723de4563ce9ef6c9c850fe2e376461
Ranadeep Mukherjee <10173000+r4ndm@users.noreply.github.com>
2025-01-25 22:46:52 -0600

    remove navbar filter list hardcoding (#11)
    Co-authored-by: Ranadeep Mukherjee <rmukherjee@slb.com>

--- angular/apps/jcc/src/app/nav-header/nav-header.component.ts
+++ angular/apps/jcc/src/app/nav-header/nav-header.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CatalogService } from '../catalog/catalog.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-header',
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './nav-header.component.html',
   styleUrl: './nav-header.component.css'
 })
 export class NavHeaderComponent {
+  constructor(private catalogService: CatalogService) {}
 
+  protected get filters(): string[] {
+    return this.catalogService.getCategories();
+  }
 }
