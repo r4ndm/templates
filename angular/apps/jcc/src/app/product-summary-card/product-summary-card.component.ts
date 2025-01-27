3d26c4a84cdc6b25c5ab7b60630e739cb846c768
Ranadeep Mukherjee <10173000+r4ndm@users.noreply.github.com>
2025-01-27 17:02:17 -0600

    use bootstrap card for product summary (#13)
    Co-authored-by: Ranadeep Mukherjee <rmukherjee@slb.com>

---
+++ angular/apps/jcc/src/app/product-summary-card/product-summary-card.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IProduct } from '../catalog/product.interface';
+import { CartService } from '../cart/cart.service';
+
+@Component({
+  selector: 'app-product-summary-card',
+  imports: [],
+  templateUrl: './product-summary-card.component.html',
+  styleUrl: './product-summary-card.component.css'
+})
+export class ProductSummaryCardComponent {
+  @Input() public product!: IProduct;
+  @Output() public detailButtonPressed = new EventEmitter<IProduct>();
+
+  constructor(private cartService: CartService) {}
+  
+  public getImageUrl(): string {
+    return `/images/products/${this.product.image}`;
+  }
+
+  public detailButtonClicked(): void {
+    this.detailButtonPressed.emit(this.product);
+  }
+
+  public addCartButtonClicked(): void {
+    //TODO: add using Id instead of product
+    if (this.product) {
+      this.cartService.add(this.product);
+    }
+  }
+}
