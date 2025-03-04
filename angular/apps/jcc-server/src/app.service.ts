ae7081b5f8c070fa69414d012fe15099aeacdfdb
Ranadeep Mukherjee <10173000+r4ndm@users.noreply.github.com>
2025-03-03 18:16:00 -0600

    enable swagger in jcc-server (#26)
    Co-authored-by: Ranadeep Mukherjee <rmukherjee@slb.com>

--- angular/apps/jcc-server/src/app.service.ts
+++ angular/apps/jcc-server/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  public getHello(): string {
-    return 'Hello World!';
+  public getVersion(): string {
+    return 'Catalog service v1.0';
   }
 }
--- angular/apps/jcc-server/src/main.ts
+++ angular/apps/jcc-server/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
-  // TODO: setup swagger
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Catalog service')
+    .setDescription('Catalog creation and retrieval APIs')
+    .setVersion('1.0')
+    .addTag('catalog')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
